c03871c7b0a6587d66044211c513affb4b42da9f
JamesLin <James@smartcloud.com.tw>
2018-04-23 16:27:14 +0800

    First Commit

---
+++ RecordSubs/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecordSubs
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Hyperlink source = sender as Hyperlink;
+            Process.Start(new ProcessStartInfo(source.NavigateUri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RecordSubs/TwitchIRC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+
+namespace WebApplication.Models
+{
+    public class TwitchIRC
+    {
+        FileStream fs = new FileStream("SubsLog.txt", FileMode.OpenOrCreate);
+        TwitchClient client;
+        public void Start(string username, string oauth, string channelname)
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(username, oauth);
+
+            client = new TwitchClient();
+            client.Initialize(credentials, channelname);
+
+            client.OnReSubscriber += onReSubscriber;
+            client.OnNewSubscriber += onNewSubscriber;
+
+            client.Connect();
+        }
+
+        private void onReSubscriber(object sender, OnReSubscriberArgs e)
+        {
+            string text = "[" + DateTime.Now.ToLongDateString() + "]" + " " + e.ReSubscriber.DisplayName + " " + e.ReSubscriber.SubscriptionPlanName + " " + e.ReSubscriber.Months + "\n";
+            byte[] by = Encoding.UTF8.GetBytes(text);
+            fs.Write(by, 0, by.Length);
+            fs.Close();
+        }
+        
+        private void onNewSubscriber(object sender, OnNewSubscriberArgs e)
+        {
+            string text = "[" + DateTime.Now.ToLongDateString() + "]" + " " + e.Subscriber.DisplayName + " " + e.Subscriber.SubscriptionPlanName + "\n";
+            byte[] by = Encoding.UTF8.GetBytes(text);
+            fs.Write(by, 0, by.Length);
+            fs.Close();
+        }
+    }
+}
