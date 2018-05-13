0e948cabfaaeae156d10e079a2c06f033f0e5731
JamesLin <j835111@gmail.com>
2018-05-14 01:02:51 +0800

    未寫完

--- RecordSubs/TwitchIRC.cs
+++ RecordSubs/TwitchIRC.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
@@ -9,7 +8,7 @@ namespace WebApplication.Models
 {
     public class TwitchIRC
     {
-        FileStream fs = new FileStream("SubsLog.txt", FileMode.OpenOrCreate);
+        StreamWriter writer = new StreamWriter("SubsLog.txt", true);
         TwitchClient client;
         public void Start(string username, string oauth, string channelname)
         {
@@ -26,18 +25,18 @@ namespace WebApplication.Models
 
         private void onReSubscriber(object sender, OnReSubscriberArgs e)
         {
-            string text = "[" + DateTime.Now + "]" + " " + e.ReSubscriber.DisplayName + " " + e.ReSubscriber.SubscriptionPlanName + " " + e.ReSubscriber.Months + "\n";
-            byte[] by = Encoding.UTF8.GetBytes(text);
-            fs.Write(by, 0, by.Length);
-            fs.Close();
+            string text = "[" + DateTime.Now + "]" + " " + e.ReSubscriber.DisplayName + "(" + e.ReSubscriber.UserId + ")" + " " + e.ReSubscriber.SubscriptionPlanName + " " + e.ReSubscriber.Months + "\n";
+            writer.WriteLine(text);
+            writer.Flush();
+            writer.Close();
         }
         
         private void onNewSubscriber(object sender, OnNewSubscriberArgs e)
         {
-            string text = "[" + DateTime.Now.ToLongDateString() + "]" + " " + e.Subscriber.DisplayName + " " + e.Subscriber.SubscriptionPlanName + "\n";
-            byte[] by = Encoding.UTF8.GetBytes(text);
-            fs.Write(by, 0, by.Length);
-            fs.Close();
+            string text = "[" + DateTime.Now + "]" + " " + e.Subscriber.DisplayName + "(" + e.Subscriber.UserId + ")" + " " + e.Subscriber.SubscriptionPlanName + "\n";
+            writer.WriteLine(text);
+            writer.Flush();
+            writer.Close();
         }
     }
 }
