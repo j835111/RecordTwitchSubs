e48d514b718995e5a48033c3814ef485989d5c88
JamesLin <James@smartcloud.com.tw>
2018-06-25 17:00:07 +0800

    回家測

--- RecordSubs/MainWindow.xaml.cs
+++ RecordSubs/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ServiceStack.Text;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +23,7 @@ namespace RecordSubs
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool ButtonSate { get; set; } = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,10 +38,23 @@ namespace RecordSubs
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            TwitchIRC irc = new TwitchIRC();
-            irc.Start(username.Text, twitchoauth.Text, channelname.Text);
-            state.Text = "Twitch聊天室連接中...\n";
-            state.Text += "訂閱紀錄中...";
+            if (!ButtonSate)
+            {
+                if (!File.Exists("SubsLog.csv"))
+                {
+                    string[] item = { "時間", "名稱", "ID", "訂閱計畫", "月數" };
+                    StreamWriter writer = new StreamWriter("SubsLog.csv", true, new UTF8Encoding(true));
+                    writer.Write(CsvSerializer.SerializeToCsv<string>(item));
+                    writer.Flush();
+                    writer.Close();
+                }                    
+                TwitchIRC irc = new TwitchIRC();
+                irc.Start(username.Text, twitchoauth.Text, channelname.Text);
+                state.Text = "Twitch聊天室連接中...\n";
+                state.Text += "訂閱紀錄中...";
+                ButtonSate = true;
+            }
+            
         }
     }
 }
--- RecordSubs/TwitchIRC.cs
+++ RecordSubs/TwitchIRC.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
+using ServiceStack.Text;
 
 namespace RecordSubs
 {
     public class TwitchIRC
     {
-        StreamWriter writer = new StreamWriter("SubsLog.txt", true);
+        StreamWriter writer = new StreamWriter("SubsLog.csv", true, new UTF8Encoding(true));     
         TwitchClient client;
         public void Start(string username, string oauth, string channelname)
         {
@@ -25,16 +27,16 @@ namespace RecordSubs
 
         private void onReSubscriber(object sender, OnReSubscriberArgs e)
         {
-            string text = "[" + DateTime.Now + "]" + " " + e.ReSubscriber.DisplayName + "(" + e.ReSubscriber.UserId + ")" + " " + e.ReSubscriber.SubscriptionPlanName + " " + e.ReSubscriber.Months + "\n";
-            writer.WriteLine(text);
+            string[] text = { DateTime.Now.ToString(), e.ReSubscriber.DisplayName, e.ReSubscriber.Login, e.ReSubscriber.SubscriptionPlanName, e.ReSubscriber.Months.ToString() };
+            writer.Write(CsvSerializer.SerializeToCsv<string>(text));
             writer.Flush();
             writer.Close();
         }
         
         private void onNewSubscriber(object sender, OnNewSubscriberArgs e)
         {
-            string text = "[" + DateTime.Now + "]" + " " + e.Subscriber.DisplayName + "(" + e.Subscriber.UserId + ")" + " " + e.Subscriber.SubscriptionPlanName + "\n";
-            writer.WriteLine(text);
+            string[] text = { DateTime.Now.ToString(), e.Subscriber.DisplayName, e.Subscriber.Login, e.Subscriber.SubscriptionPlanName.ToString() };
+            writer.Write(CsvSerializer.SerializeToCsv<string>(text));
             writer.Flush();
             writer.Close();
         }
